e0363f6aab573c01e7fcf79e9be4aa1932f3aae6
BuildTools <unconfigured@null.spigotmc.org>
2021-10-06 19:19:00 +0200

    6/10/2021

--- esercizio2/src/it/unicam/cs/asdl2122/es2/CombinationLock.java
+++ esercizio2/src/it/unicam/cs/asdl2122/es2/CombinationLock.java
@@ -5,40 +5,53 @@ package it.unicam.cs.asdl2122.es2;
  * impostata su certe posizioni contrassegnate da lettere maiuscole. La
  * serratura si apre solo se le ultime tre lettere impostate sono uguali alla
  * combinazione segreta.
- * 
+ *
  * @author Luca Tesei
  */
 public class CombinationLock {
 
     // TODO inserire le variabili istanza che servono
+    String combinazione;
+    StringBuffer combinazioneInserita = new StringBuffer();
+
+    private boolean locked = true;
 
     /**
      * Costruisce una cassaforte <b>aperta</b> con una data combinazione
-     * 
-     * @param aCombination
-     *                         la combinazione che deve essere una stringa di 3
-     *                         lettere maiuscole dell'alfabeto inglese
-     * @throw IllegalArgumentException se la combinazione fornita non è una
-     *        stringa di 3 lettere maiuscole dell'alfabeto inglese
-     * @throw NullPointerException se la combinazione fornita è nulla
+     *
+     * @param aCombination la combinazione che deve essere una stringa di 3
+     *                     lettere maiuscole dell'alfabeto inglese
+     * @throws IllegalArgumentException se la combinazione fornita non è una
+     * stringa di 3 lettere maiuscole dell'alfabeto inglese
+     * @throws NullPointerException se la combinazione fornita è nulla
      */
     public CombinationLock(String aCombination) {
-        // TODO implementare
+        char[] caratteriCombinazione = aCombination.toCharArray();
+        if (aCombination.length() != 3 || !Character.isLetter(caratteriCombinazione[0]) ||
+                Character.isLowerCase(caratteriCombinazione[0]) || !Character.isLetter(caratteriCombinazione[1]) ||
+                Character.isLowerCase(caratteriCombinazione[1]) || !Character.isLetter(caratteriCombinazione[2]) ||
+                Character.isLowerCase(caratteriCombinazione[2]))
+            throw new IllegalArgumentException("La combinazione deve avere 3 lettere maiuscole");
+        if (aCombination.equals(null)) throw new NullPointerException("La stringa inserita è nulla");
+
+        combinazione = aCombination;
     }
 
     /**
-     * Imposta la manopola su una certaposizione.
-     * 
-     * @param aPosition
-     *                      un carattere lettera maiuscola su cui viene
-     *                      impostata la manopola
-     * @throws IllegalArgumentException
-     *                                      se il carattere fornito non è una
-     *                                      lettera maiuscola dell'alfabeto
-     *                                      inglese
+     * Imposta la manopola su una certa posizione.
+     *
+     * @param aPosition un carattere lettera maiuscola su cui viene
+     *                  impostata la manopola
+     * @throws IllegalArgumentException se il carattere fornito non è una
+     *                                  lettera maiuscola dell'alfabeto
+     *                                  inglese
      */
     public void setPosition(char aPosition) {
-        // TODO implementare
+        if (!Character.isLetter(aPosition) || Character.isLowerCase(aPosition)) throw new IllegalArgumentException(
+                "Il carattere fornito non è una lettera maiuscola");
+        if (combinazioneInserita.length() < 3) {
+            combinazioneInserita.append(aPosition);
+        }
     }
 
     /**
@@ -48,17 +61,20 @@ public class CombinationLock {
      * prossimi tentativi di apertura.
      */
     public void open() {
-        // TODO implementare
+        if (combinazioneInserita.toString().equals(combinazione)) {
+            locked = true;
+        } else {
+            combinazioneInserita.delete(0, 3);
+        }
     }
 
     /**
      * Determina se la cassaforte è aperta.
-     * 
+     *
      * @return true se la cassaforte è attualmente aperta, false altrimenti
      */
     public boolean isOpen() {
-        // TODO implementare
-        return false;
+        return locked;
     }
 
     /**
@@ -69,7 +85,8 @@ public class CombinationLock {
      * sono proprio la combinazione attuale.
      */
     public void lock() {
-        // TODO implementare
+        locked = false;//cassaforte chiusa
+        combinazioneInserita.delete(0, 3);
     }
 
     /**
@@ -78,15 +95,28 @@ public class CombinationLock {
      * rimane chiusa e la combinazione non viene cambiata, ma in questo caso le
      * le lettere impostate precedentemente non devono essere considerate per i
      * prossimi tentativi di apertura.
-     * 
-     * @param aCombination
-     *                         la nuova combinazione che deve essere una stringa
-     *                         di 3 lettere maiuscole dell'alfabeto inglese
-     * @throw IllegalArgumentException se la combinazione fornita non è una
-     *        stringa di 3 lettere maiuscole dell'alfabeto inglese
-     * @throw NullPointerException se la combinazione fornita è nulla
+     *
+     * @param aCombination la nuova combinazione che deve essere una stringa
+     *                     di 3 lettere maiuscole dell'alfabeto inglese
+     * @throws IllegalArgumentException se la combinazione fornita non è una
+     * stringa di 3 lettere maiuscole dell'alfabeto inglese
+     * @throws NullPointerException se la combinazione fornita è nulla
      */
     public void lockAndChangeCombination(String aCombination) {
-        // TODO implementare
+        if (locked) {
+            //controllo aCombination
+            if(aCombination == null) throw new NullPointerException("Nuova combinazione nulla");
+            if (aCombination.length() != 3) throw new IllegalArgumentException("La nuova combinazione deve essere di " +
+                    "tre caratteri");
+            char[] caratteriCombinazione = aCombination.toCharArray();
+            for (int i = 0; i < 3; i++) {
+                if(!Character.isLetter(caratteriCombinazione[i]) || Character.isLowerCase(caratteriCombinazione[i]))
+                    throw new IllegalArgumentException("La nuova combinazione devono essere tre lettere maiuscole");
+            }
+            combinazione = aCombination;
+            lock();
+        } else {
+            combinazioneInserita.delete(0, 3);
+        }
     }
 }
